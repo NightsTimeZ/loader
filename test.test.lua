d4e639b6effe590015e31278be0142f5aabac4bc
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-10-29 13:17:55 +0700

    Update test.test.lua

--- test.test.lua
+++ test.test.lua
@@ -1,6 +1,6 @@
 local gamelist = {
 	[994732206] = "https://raw.githubusercontent.com/NightsTimeZ/loader/refs/heads/main/bb.lua",
-  [111111] = "https://raw.githubusercontent.com/NightsTimeZ/loader/refs/heads/main/dve.lua"
+  [1202096104] = "https://raw.githubusercontent.com/NightsTimeZ/loader/refs/heads/main/dve.lua"
 }
 local checkgame = gamelist[game.gameId]
 
