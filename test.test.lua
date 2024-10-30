199744d955857be32c18606a11aae8e5e558eb1a
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-10-30 14:36:35 +0700

    dad

--- test.test.lua
+++ test.test.lua
@@ -1,10 +1,10 @@
 local gamelist = {
-	[994732206] = "https://raw.githubusercontent.com/NightsTimeZ/loader/refs/heads/main/bb.lua",
-  [1202096104] = "https://raw.githubusercontent.com/NightsTimeZ/loader/refs/heads/main/dve.lua"
+	[4777817887] = "https://raw.githubusercontent.com/NightsTimeZ/loader/refs/heads/main/bb.lua",
+    [1202096104] = "https://raw.githubusercontent.com/NightsTimeZ/loader/refs/heads/main/dve.lua"
 }
 local checkgame = gamelist[game.gameId]
 
-if gamelist[game.gameId] then
+if checkgame then
 	loadstring(game:HttpGet(checkgame))()
 else
 	game.Players.LocalPlayer:Kick("Game Not Support")
